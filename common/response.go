37b4946f177c1a2f44696e095e9a71b71f5cd997
Ivan Mirić <ivan.miric@grafana.com>
2022-03-22 16:37:01 +0100

    Add some logs

--- common/response.go
+++ common/response.go
@@ -149,6 +149,9 @@ func (r *Response) fetchBody() error {
 	if cached() {
 		return nil
 	}
+	r.logger.Debugf("Response:fetchBody",
+		"url:%s method:%s rid:%s", r.url, r.request.method, string(r.request.requestID))
+
 	action := network.GetResponseBody(r.request.requestID)
 	body, err := action.Do(cdp.WithExecutor(r.ctx, r.request.frame.manager.session))
 	if err != nil {
