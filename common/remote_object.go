6e0cdcedab4809a5ba6a60a097caf30a62ff80fd
Ivan Mirić <ivan.miric@grafana.com>
2022-06-30 17:49:20 +0200

    Remove "unable to" from log messages

--- common/remote_object.go
+++ common/remote_object.go
@@ -164,7 +164,7 @@ func handleParseRemoteObjectErr(ctx context.Context, err error, logger *logrus.E
 	merr, ok := err.(*multierror.Error)
 	if !ok {
 		// If this panics it's a bug :)
-		k6ext.Panic(ctx, "unable to parse remote object value: %w", err)
+		k6ext.Panic(ctx, "parsing remote object value: %w", err)
 	}
 	for _, e := range merr.Errors {
 		switch {
@@ -174,7 +174,7 @@ func handleParseRemoteObjectErr(ctx context.Context, err error, logger *logrus.E
 			logger.WithError(ope).Error()
 		default:
 			// If this panics it's a bug :)
-			k6ext.Panic(ctx, "unable to parse remote object value: %w", e)
+			k6ext.Panic(ctx, "parsing remote object value: %w", e)
 		}
 	}
 }
