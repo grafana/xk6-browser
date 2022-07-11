9899bbad343a854616fcf1cc95a8c2a3aea78609
Ivan Mirić <ivan.miric@grafana.com>
2022-07-11 18:23:03 +0200

    wip: watcher

--- cdp/watcher.go
+++ cdp/watcher.go
@@ -1,9 +1,14 @@
 package cdp
 
-type EventWatcher struct {
+import "github.com/chromedp/cdproto"
+
+type Watcher struct {
 	// TODO: Some other extensible way of indexing subscribers?
 	subs map[string]map[string]chan<- Event
 }
 
-func (ew *EventWatcher) Subscribe(sessionID, frameID string, evt Event) (<-chan Event, error) {
+func (w *Watcher) SubscribeToEvent(sessionID, frameID string, evt Event) (<-chan Event, error) {
+}
+
+func (w *Watcher) SubscribeToMessage(msgID int64) (<-chan *cdproto.Message, error) {
 }
