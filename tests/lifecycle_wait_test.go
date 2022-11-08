340f76b31933848a65caa926993d1d02fff37588
ankur22 <ankur.agarwal@grafana.com>
2022-11-08 14:58:34 +0000

    Add a test for goto and wait on domcontentloaded
    This test ensure that we can navigate to a page and wait for the
    domcontentloaded lifecycle event. The test ensures that we're not
    waiting on the async scripts and other network requests to complete
    before we unblock on the wait.

--- tests/lifecycle_wait_test.go
+++ tests/lifecycle_wait_test.go
@@ -373,6 +373,52 @@ func TestLifecycleReloadNetworkIdle(t *testing.T) {
 	})
 }
 
+func TestLifecycleDOMContentLoaded(t *testing.T) {
+	// Test description
+	//
+	// 1. goto /home and wait for the domcontentloaded lifecycle event.
+	//
+	// Success criteria: Once the HTML is loaded it should unblock, which
+	//                   will unblock before the async script and other
+	//                   network requests complete.
+
+	t.Parallel()
+
+	tb := newTestBrowser(t, withFileServer())
+	p := tb.NewPage(nil)
+	tb.withHandler("/home", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, tb.staticURL("wait_for_nav_lifecycle.html"), http.StatusMovedPermanently)
+	})
+
+	var counter int64
+	var counterMu sync.Mutex
+	tb.withHandler("/ping", func(w http.ResponseWriter, _ *http.Request) {
+		counterMu.Lock()
+		defer counterMu.Unlock()
+
+		time.Sleep(time.Millisecond * 100)
+
+		counter++
+		fmt.Fprintf(w, "pong %d", counter)
+	})
+
+	tb.withHandler("/ping.js", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintf(w, `
+				await new Promise(resolve => setTimeout(resolve, 1000));
+				var pingJSTextOutput = document.getElementById("pingJSText");
+				pingJSTextOutput.innerText = "ping.js loaded from server";
+			`)
+	})
+
+	assertHome(t, tb, p, common.LifecycleEventDOMContentLoad, func() {
+		result := p.TextContent("#pingRequestText", nil)
+		assert.NotEqualValues(t, "Waiting... pong 10 - for loop complete", result)
+
+		result = p.TextContent("#pingJSText", nil)
+		assert.EqualValues(t, "Waiting...", result)
+	})
+}
+
 func TestLifecycleNetworkIdle(t *testing.T) {
 	t.Parallel()
 
