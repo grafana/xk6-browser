043a5d28bf848caf1a5d9d10d1c648e73e525c80
ankur22 <ankur.agarwal@grafana.com>
2023-02-08 15:15:49 +0000

    Add a defaultRunID
    This will avoid any confusion when debugging the register -- an
    empty RunID in the register might confuse devs, whereas default
    shouldn't and can be tracked to where it was set.
    
    Resolves: https://github.com/grafana/xk6-browser/pull/755#issuecomment-1422714504

--- osext/context.go
+++ osext/context.go
@@ -10,6 +10,10 @@ const (
 	ctxKeyRunID ctxKey = iota
 )
 
+const (
+	defaultRunID = "default"
+)
+
 // WithRunID saves the current iteration ID to the context.
 func WithRunID(ctx context.Context, rID string) context.Context {
 	return context.WithValue(ctx, ctxKeyRunID, rID)
@@ -18,5 +22,8 @@ func WithRunID(ctx context.Context, rID string) context.Context {
 // GetRunID returns the current iteration ID from the context.
 func GetRunID(ctx context.Context) string {
 	rID, _ := ctx.Value(ctxKeyRunID).(string)
+	if rID == "" {
+		rID = defaultRunID
+	}
 	return rID
 }
