de5f6bec23fe0c693db6f430981e2dd95a0d476a
ankur22 <ankur.agarwal@grafana.com>
2024-01-29 15:49:34 +0000

    fixup! Add a new FilePersister interface

--- storage/file_persister.go
+++ storage/file_persister.go
@@ -3,12 +3,10 @@ package storage
 import (
 	"context"
 	"io"
-
-	"github.com/grafana/xk6-browser/log"
 )
 
 // FilePersister will persist files. It abstracts away the where and how of
 // writing files to the source destination.
 type FilePersister interface {
-	Persist(ctx context.Context, logger *log.Logger, path string, data io.Reader) error
+	Persist(ctx context.Context, path string, data io.Reader) error
 }
