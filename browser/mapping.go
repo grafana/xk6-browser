132e7ef9049c24f380487a411f228ac88c939c71
ankur22 <ankur.agarwal@grafana.com>
2024-04-04 17:08:51 +0100

    Add a test function
    This tests the functionality of self. It is an api on locator which
    takes a locator as an argument. It works with self to retrieve the
    reference to the go object of the passed in locator argument.

--- browser/mapping.go
+++ browser/mapping.go
@@ -87,6 +87,17 @@ func mapLocator(vu moduleVU, lo *common.Locator) mapping {
 		"type":         lo.Type,
 		"hover":        lo.Hover,
 		"tap":          lo.Tap,
+		"test": func(l2 goja.Value) (string, error) {
+			m := mapping{}
+			err := rt.ExportTo(l2, &m)
+			if err != nil {
+				return "", err
+			}
+
+			realL2 := m["self"].(func() *common.Locator)()
+
+			return lo.Test(realL2)
+		},
 		"dispatchEvent": func(typ string, eventInit, opts goja.Value) error {
 			popts := common.NewFrameDispatchEventOptions(lo.DefaultTimeout())
 			if err := popts.Parse(vu.Context(), opts); err != nil {
