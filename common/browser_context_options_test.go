1e04deed5211d100167d8a5694ec6bbbba4a345e
ankur22 <ankur.agarwal@grafana.com>
2024-10-25 12:15:36 +0200

    Add all permissions to test which ones work in CI

--- common/browser_context_options_test.go
+++ common/browser_context_options_test.go
@@ -15,9 +15,39 @@ func TestBrowserContextOptionsPermissions(t *testing.T) {
 	err := opts.Parse(vu.Context(), vu.ToSobekValue((struct {
 		Permissions []any `js:"permissions"`
 	}{
-		Permissions: []any{"camera", "microphone"},
+		Permissions: []any{"accelerometer",
+			"accessibility-events",
+			"ambient-light-sensor",
+			"background-sync",
+			"camera",
+			"clipboard-read",
+			"clipboard-write",
+			"geolocation",
+			"gyroscope",
+			"magnetometer",
+			"microphone",
+			"midi-sysex",
+			"midi",
+			"notifications",
+			"payment-handler",
+			"storage-access"},
 	})))
 	assert.NoError(t, err)
-	assert.Len(t, opts.Permissions, 2)
-	assert.Equal(t, opts.Permissions, []string{"camera", "microphone"})
+	assert.Len(t, opts.Permissions, 16)
+	assert.Equal(t, opts.Permissions, []string{"accelerometer",
+		"accessibility-events",
+		"ambient-light-sensor",
+		"background-sync",
+		"camera",
+		"clipboard-read",
+		"clipboard-write",
+		"geolocation",
+		"gyroscope",
+		"magnetometer",
+		"microphone",
+		"midi-sysex",
+		"midi",
+		"notifications",
+		"payment-handler",
+		"storage-access"})
 }
